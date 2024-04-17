332540eaa1f88ede14e5a9f197b84dc1492a43e9
lizhuit <lizbethhuitzil28@gmail.com>
2024-04-17 17:17:22 -0600

    Datos Personales

--- src/gestor/empresarial/datos/DatosPersonales.java
+++ src/gestor/empresarial/datos/DatosPersonales.java
@@ -8,6 +8,7 @@ public abstract class DatosPersonales {
     private String correo;
     private String whatsapp;
 
+
     //getId() y setId() permiten obtener y establecer su valor de la variable.
     protected int getId() {
         return id;
@@ -16,31 +17,31 @@ public abstract class DatosPersonales {
         this.id = id;
     }
     //getNombre() y setNombre() permiten obtener y establecer su valor de la variable.
-    protected String getNombre() {
+    public String getNombre() {
         return nombre;
     }
     protected void setNombre(String nombre) {
-        // this.nombre = nombre;
+         this.nombre = nombre;
     }
     //getApellido() y setApellido() permiten obtener y establecer su valor de la variable.
     protected String getApellidos() {
         return apellidos;
     }
     protected void setApellidos(String apellidos) {
-        // this.apellidos = apellidos;
+        this.apellidos = apellidos;
     }
     //getCorreo() y setCorreo() permiten obtener y establecer su valor de la variable.
     protected String getCorreo() {
         return correo;
     }
     protected void setCorreo(String correo) {
-       // this.correo = correo;
+       this.correo = correo;
     }
     //getWhatsapp() y setWhatsapp() permiten obtener y establecer su valor de la variable.
     protected String getWhatsapp() {
         return whatsapp;
     }
     protected void setWhatsapp(String whatsapp) {
-        //this.whatsapp = whatsapp;
+        this.whatsapp = whatsapp;
     }
 }
