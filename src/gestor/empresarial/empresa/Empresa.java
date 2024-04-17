1dfdde73c891800334c955c792d717034cc1873b
lizhuit <lizbethhuitzil28@gmail.com>
2024-04-17 17:31:00 -0600

    Empresa

--- src/gestor/empresarial/empresa/Empresa.java
+++ src/gestor/empresarial/empresa/Empresa.java
@@ -5,17 +5,27 @@ import gestor.errores.GestionErrores;
 //Declara la clase Empresa como final, lo que significa que no se puede heredar de ella.
 public final class Empresa {
     //Atributos
-    private String nombreEmpresa;
-    private String representanteLegal;
-    private String telefono;
-    private String rfc;
+    private String nombreEmpresa = "Itera Process";
+    private String representanteLegal = "Roberto García";
+    private String telefono = "222 298 6412";
+    private String rfc = "GARO280698P71";
     public Empleados datosRH;
     public GestionErrores error;
+
     //Constructor de Empresa con paso de parámetros
     public Empresa (String a, String b){
 
     }
     //Método setRepresentanteLegal
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
     public void setRepresentanteLegal(String representanteLegal) {
         this.representanteLegal = representanteLegal;
     }
@@ -23,12 +33,20 @@ public final class Empresa {
     public String getRepresentanteLegal() {
         return representanteLegal;
     }
-    //Método setTelefono
+
+    public String getTelefono() {
+        return telefono;
+    }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
-    //Método getInfo, regresa teléfono
-    public String getInfo(){
-        return telefono;
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
     }
 }
