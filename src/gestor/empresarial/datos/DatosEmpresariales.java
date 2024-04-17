a413082736a0dda47e97604d19fcf51e33c455b5
lizhuit <lizbethhuitzil28@gmail.com>
2024-04-17 17:16:03 -0600

    Datos Empresariales

--- src/gestor/empresarial/datos/DatosEmpresariales.java
+++ src/gestor/empresarial/datos/DatosEmpresariales.java
@@ -1,14 +1,16 @@
 package gestor.empresarial.datos;
 
-public final class DatosEmpresariales extends  DatosPersonales{
+public final class DatosEmpresariales {
     //Atributos de la clase
     private String adscripcion;
     private String telefonoExterior;
     private String puesto;
 
     //Constructor
-    public DatosEmpresariales(int a, String b, String c, String d){
-
+    public DatosEmpresariales(String adscripcion, String telefonoExterior, String puesto){
+        this.adscripcion=adscripcion;
+        this.telefonoExterior=telefonoExterior;
+        this.puesto=puesto;
     }
 
     //getAdscripcion() y setAdscripcion() permiten obtener y establecer su valor de la variable.
@@ -16,20 +18,20 @@ public final class DatosEmpresariales extends  DatosPersonales{
         return adscripcion;
     }
     void setAdscripcion(String adscripcion) {
-        //this.adscripcion = adscripcion;
+        this.adscripcion = adscripcion;
     }
     //getTelefonoExterior() y setTelefonoExterior() permiten obtener y establecer su valor de la variable.
     String getTelefonoExterior() {
         return telefonoExterior;
     }
     void setTelefonoExterior(String telefonoExterior) {
-        //this.telefonoExterior = telefonoExterior;
+        this.telefonoExterior = telefonoExterior;
     }
     //getPuesto() y setPuesto() permiten obtener y establecer su valor de la variable.
     String getPuesto() {
         return puesto;
     }
     void setPuesto(String puesto) {
-        //this.puesto = puesto;
+        this.puesto = puesto;
     }
 }
