9e2770ca229d9f039764a3d61c15159d4677f9a7
AdriTH9 <tlaxcaltecatladriana40@gmail.com>
2024-04-18 20:14:28 -0600

    UniversionedFile... creeo q este es el rollo de los ERORR de Java

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,4 @@
-import gestor.interfaz.Loguin;//importamos la interfaz gráfica
+import gestor.interfaz.*;//importamos la interfaz gráfica
 public class Principal {
     public static void main(String []args){
         Loguin log = new Loguin();
