9d18261aed50254bf56fda6647f581952257ce14
lizhuit <lizbethhuitzil28@gmail.com>
2024-04-19 14:30:05 -0600

    Main para que yo pueda correr el programa

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import gestor.empresarial.contrato.Contrato;
+import gestor.interfaz.*;//importamos la interfaz gráfica
+public class Main {
+    public static void main(String []args){
+        Loguin log = new Loguin();
+
+    }
+}
