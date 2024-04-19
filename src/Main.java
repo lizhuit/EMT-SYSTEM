ddaa4c11d456e130390d2042fc6b31f909a8a68e
lizhuit <lizbethhuitzil28@gmail.com>
2024-04-19 15:23:38 -0600

    eliminar

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import gestor.empresarial.contrato.Contrato;
-import gestor.interfaz.*;//importamos la interfaz gráfica
-public class Main {
-    public static void main(String []args){
-        Loguin log = new Loguin();
-
-    }
-}
