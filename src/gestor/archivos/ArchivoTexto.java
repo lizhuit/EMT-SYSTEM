213430a5f45133a5ad1d80090b94469616301b49
lizhuit <lizbethhuitzil28@gmail.com>
2024-04-19 13:36:10 -0600

    Comentarios-Archivos

--- src/gestor/archivos/ArchivoTexto.java
+++ src/gestor/archivos/ArchivoTexto.java
@@ -10,50 +10,49 @@ public final class ArchivoTexto extends ControlArchivos implements iFileText{//e
     /*Lo q contiene este, lo entrega al buffered*/
 
     private BufferedReader br;/*buffered, es un pedazo de memoria, y
-     si necesita más pide más al procesador y va toamndo de apocos.
-     es practicamente un arreglo y puede cargar grandes masas de info*/
+     si necesita más pide más al procesador y va tomando de apocos.
+     es practicamente un arreglo y puede cargar grandes masas de información*/
 
     private FileWriter fw;//prepara el archivo para escritura
-    private BufferedWriter bw;/*comunicacion, de todoo lo qu eesta en el reader, lo entrega/guarda al archivo*/
+    private BufferedWriter bw;/*comunicacion, de todoo lo qu esta en el reader, lo entrega/guarda al archivo*/
     private boolean archivoExistente;
     private boolean modoLectura;
     private boolean modoEscritura;
 
 
-    public ArchivoTexto(String arc){
-        super();//constructor
-        try{
-            file= new File(arc);
-            if(!file.exists())
-                file.createNewFile();
-
+    public ArchivoTexto(String arc){//Constructor
+        super();
+        try{//Puede generar una exepicón
+            file= new File(arc); //Crea un objeto "file"
+            if(!file.exists())//Comprueba si un archivo especifico no existe
+                file.createNewFile();//Sino existe, crea uno con ese nombre
+            //Variables instanciadas
             this.archivoExistente=true;
             this.modoLectura=false;
             this.modoEscritura=false;
         }
-        catch(Exception e){
-            System.out.println("NO SE ENCUENTRA EL ARCHIVO");
+        catch(Exception e){//Captura alguna excepción
+            System.out.println("NO SE ENCUENTRA EL ARCHIVO"); //Si ocurre alguna excepción manda un mensaje
             this.archivoExistente=false;
         }
     }
     public ArchivoTexto(String b, boolean c){//constructor
     }
-    public void AbrirModoLectura(){
-        if(archivoExistente==true) {
+    public void AbrirModoLectura(){//Método AbrirModoLectura
+        if(archivoExistente==true) { //Verifica que la variable es verdadera
             try {
                 //con estas dos instancias el archivo esta en uso:
-                fr = new FileReader(this.file.getAbsoluteFile());
-                br = new BufferedReader(this.fr);
-
-                this.modoLectura = true;
-                System.out.println("ARCHIVO ABIERTO EN MODO LECTURA");
-            } catch (Exception e) {
-                System.out.println("ERROR: EL ARCHIVO NO SE PUEDE ABIRIR EN MODO LECTURA");
+                fr = new FileReader(this.file.getAbsoluteFile());//objeto para leer el archivo como un secuencia de caracteres
+                br = new BufferedReader(this.fr);//objeto que lee el archivo de una menera más eficiente utilizando un búfer
+                this.modoLectura = true;// si el archivo se abre correctamente
+                System.out.println("ARCHIVO ABIERTO EN MODO LECTURA");//indica que está en modo lectura
+            } catch (Exception e) {//Captura excepciones
+                System.out.println("ERROR: EL ARCHIVO NO SE PUEDE ABIRIR EN MODO LECTURA");//mensaj de error
             }
         }
     }
-    public String Leer(){
-        if(archivoExistente==true) {
+    public String Leer(){//Método Leer
+        if(archivoExistente==true) {//Verifica que el archivo existe
             try {
                 return this.br.readLine();//si no hay error retorna esto y ya no ejecuta lo que falta de la funcion
             } catch (Exception e) {
@@ -62,9 +61,9 @@ public final class ArchivoTexto extends ControlArchivos implements iFileText{//e
         }
         return null;
     }
-    public void AbrirModoEscritura(){
+    public void AbrirModoEscritura(){//Método AbrirModoEscritura
 
-        if(archivoExistente==true) {
+        if(archivoExistente==true) {//Verifica que el archivo exista
             try {
                 //con estas dos instancias el archivo esta en uso:
                 fw = new FileWriter(this.file.getAbsoluteFile(),true);
@@ -79,19 +78,18 @@ public final class ArchivoTexto extends ControlArchivos implements iFileText{//e
     }
     public void Escribir(String texto){
 
-        if(archivoExistente==true){
+        if(archivoExistente==true){//Verifica que exista
             try{
                 this.bw.write(texto+"\n");//escribo en el buffered no en el archivo
             }catch(Exception e){
-                System.out.println("EROOR: NO SE PUEDE ESCRIBIR INFORMACIÓN EN EL ARCHIVO");
+                System.out.println("EROOR: NO SE PUEDE ESCRIBIR INFORMACIÓN EN EL ARCHIVO");//Se imprime si existe alguna exception
             }
         }
     }
-    public void Cerrar(){
-
-        if(modoEscritura==true){
+    public void Cerrar(){//Método Cerrar
+        if(modoEscritura==true){//Verifica que esté en modo Escritura
             try{
-                this.bw.close();
+                this.bw.close();//cerrar con el bufer
                 this.fw.close();
             }catch(Exception e){ }
         }
--- src/gestor/archivos/ControlArchivos.java
+++ src/gestor/archivos/ControlArchivos.java
@@ -8,76 +8,76 @@ class ControlArchivos{//clase padre
     public ControlArchivos(){//constructor
         this.archivo = archivo; //instanciar archivo
     }
-    public boolean Crear(){//Para crear un archivo nuevo
-        File file = new File(archivo);
+    public boolean Crear(){//Método Crear
+        File file = new File(archivo); //objeto file
         try{
-            if (file.exists()){
+            if (file.exists()){// si el archivo ya exist, se imprime el mensaje
                 System.out.println("El archivo ya existe.");
                 return false;
             }
             else {
-                if (file.createNewFile()){
+                if (file.createNewFile()){ //Si no existe, se inteta crear e imprime mensaje
                     System.out.println("Se ha creado el archivo exitosamente.");
                     return false;
-                } else{
+                } else{//No se puede crear
                     System.out.println("No se puede crear el archivo.");
                     return false;
                 }
             }
-        } catch (Exception e){
+        } catch (Exception e){//Si hay errores, lo muestra
             System.out.println("ERROR: " + e.getMessage());
             return false;
         }
     }
-    public boolean Eliminar() {
-        File file = new File(archivo);
+    public boolean Eliminar(){//Método Eliminar
+        File file = new File(archivo); //Objeto file
         try {
-            if (file.exists()) {//si el archivo existe se hará lo siguiente:
-                if (file.delete()) {
+            if (file.exists()){
+                if (file.delete()) {//si el archivo existe lo elimina y muestra mensaje
                     System.out.println("El archivo ha sido eliminado exitosamente.");
                     return true;
                 }
-                else {
+                else {//existe, pero no se puede eliminar
                     System.out.println("El archivo no puede ser eliminado.");
                     return false;
                 }
             }
-            else {
+            else {//No existe el archivo
                 System.out.println("El archivo no es existente.");
                 return false;
             }
-        } catch (Exception e) {
+        } catch (Exception e) {//Si hay errores los imprime
             System.out.println("ERRROR: " + e.getMessage());
             return false;
         }
     }
 
-    public boolean Cambiar(String archivo){
+    public boolean Cambiar(String archivo){//Método Cambiar
         //creamos un objeto de ArchivoTexto
         ArchivoTexto objAT = new ArchivoTexto(archivo);
         objAT.AbrirModoEscritura();
         return true;
     }
 
-    public boolean Mover(String archivo){
-        File file = new File(archivo);
+    public boolean Mover(String archivo){//Método Mover
+        File file = new File(archivo);//Objeto file
         try{
             String ubiNew = null;
             File ubiNewArc = new File(ubiNew);
             if(file.exists()) {
-                if (file.renameTo(ubiNewArc)) {
+                if (file.renameTo(ubiNewArc)) {//Si el archivo existe, lo mueve
                     System.out.println("El archivo se ha movido exitosamente");
                     return true;
-                } else {
+                } else {//Existe, pero no se puede mover
                     System.out.println("No se puede mover el archivo.");
                     return false;
                 }
             }
-                else{
+                else{//El archivo no existe
                     System.out.println("El archivo no es existente.");
                     return false;
                 }
-            }catch (Exception e){
+            }catch (Exception e){//Muestra errores
             System.out.println("ERROR: " + e.getMessage());
             return false;
         }
